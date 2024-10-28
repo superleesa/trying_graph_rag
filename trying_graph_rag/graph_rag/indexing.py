29324571d10e98d11d5e9efae7ffbe95595ae8ba
Satoshi <6world4trigger@gmail.com>
2024-10-28 13:53:57 +0800

    add extract_entities_and_relations

--- trying_graph_rag/graph_rag/indexing.py
+++ trying_graph_rag/graph_rag/indexing.py
@@ -1,12 +1,74 @@
 import networkx as nx
 
+import ollama
 from graspologic.partition import hierarchical_leiden
 
+from trying_graph_rag.graph_rag.types import Entity, Relationship
 
-def extract_entities_and_relations(text: str) -> tuple[list[dict], list[dict]]:
-    # use the generate_relationships.txt as a prompt
-    # and use the llm to generate the entities and relationships
-    pass
+
+with open("../trying_graph_rag/graph_rag/prompts/entities_and_relationships_extraction.txt") as file:
+    PROMPT = file.read()
+
+
+def extract_entities_and_relations(document: str, entity_types: list[str], tuple_delimiter: str = r'<>', record_delimiter: str = '\n', completion_delimiter: str = '###END###') -> tuple[list[Entity], list[Relationship]]:
+    """
+    use the generate_relationships.txt as a prompt
+    and use the llm to generate the entities and relationships
+    """
+    
+    def parse_output(output: str, tuple_delimiter: str = r'<>', record_delimiter: str = '\n', completion_delimiter: str = '###END###') -> tuple[list[Entity], list[Relationship]]:
+        output = output.replace(completion_delimiter, '')
+        records = output.strip().split(record_delimiter)
+        
+        entities: list[Entity] = []
+        relationships: list[Relationship] = []
+        
+        for record in records:
+            record = record.strip().lstrip('(').rstrip(')')
+            # skip empty records
+            if not record:
+                continue
+            
+            record_content = [record_field.strip().strip("'\"") for record_field in record.split(tuple_delimiter)]
+            
+            # skip empty records pt2
+            if not len(record_content) or not all(record_content):
+                continue
+            
+            record_type = record_content[0]
+            if record_type not in ['entity', 'relationship']:
+                raise ValueError(f"Invalid record type: {record_content}")
+            
+            if record_type == 'entity' and len(record_content) == 4:
+                entity = Entity(
+                    entity_name=record_content[1],
+                    entity_type=record_content[2],
+                    entity_description=record_content[3]
+                )
+                entities.append(entity)
+            elif record_type == 'relationship' and len(record_content) == 5:
+                try:
+                    relationship_strength = int(record_content[4])
+                except ValueError:
+                    raise ValueError("Invalid relationship strength")
+                
+                relationship = Relationship(
+                    source_entity=record_content[1],
+                    target_entity=record_content[2],
+                    relationship_description=record_content[3],
+                    relationship_strength=relationship_strength
+                )
+                relationships.append(relationship)
+            else:
+                print(f"Invalid record format: {record}")
+        
+        return entities, relationships
+    
+    ollama_response = ollama.generate(model='gemma2:2b', prompt=PROMPT.format(input_text=document, entity_types=str(entity_types)[1:-1], tuple_delimiter=tuple_delimiter, record_delimiter=record_delimiter, completion_delimiter=completion_delimiter), options={"temperature": 0})
+    
+    content = ollama_response['response']
+    return parse_output(content, tuple_delimiter, record_delimiter, completion_delimiter)
+ 
 
 def summarize_entities(entities: list[dict]) -> str:
     # basically you just put description of all entities into llm
