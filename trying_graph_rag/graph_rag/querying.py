8975aa6ac7d54e9cad7cd45f482a9701861f0821
Satoshi <6world4trigger@gmail.com>
2024-10-29 15:45:22 +0800

    ruffen querying.py

--- trying_graph_rag/graph_rag/querying.py
+++ trying_graph_rag/graph_rag/querying.py
@@ -8,12 +8,14 @@ PROMPT_DIR = Path(__file__).parent / "prompts"
 
 with open(PROMPT_DIR / "map.txt") as file:
     MAP_PROMPT = file.read()
-    
+
 with open(PROMPT_DIR / "reduction.txt") as file:
     REDUCTION_PROMPT = file.read()
 
 
-def map_query_to_community(query: str, community_reports: list[SummarizedCommunity]) -> list[tuple[SummarizedCommunity, list[RelevantPointToQuery]]]:
+def map_query_to_community(
+    query: str, community_reports: list[SummarizedCommunity]
+) -> list[tuple[SummarizedCommunity, list[RelevantPointToQuery]]]:
     # for each community report, generate a partial answer using the query as context
     # calculate the relevance score of the partial answer
     # return a list of community reports with their relevance scores
@@ -23,20 +25,21 @@ def map_query_to_community(query: str, community_reports: list[SummarizedCommuni
             ollama_response = ollama_response[7:-3].strip()
         output_json = json.loads(ollama_response)
         return [RelevantPointToQuery(output_record) for output_record in output_json["points"]]
-        
-    
+
     community_partial_answers = []
-    
+
     for community_report in community_reports:
-        formatted_community_report = community_report.community_report.model_dump_json()  # just dump the community report to the prompt
+        formatted_community_report = (
+            community_report.community_report.model_dump_json()
+        )  # just dump the community report to the prompt
         ollama_response = generate_ollama_response(
             prompt=MAP_PROMPT.format(query=query, community_report=formatted_community_report),
         )
-        
+
         discovered_points = parse_output(ollama_response)
-        
+
         community_partial_answers.append((community_report, discovered_points))
-    
+
     return community_partial_answers
 
 
@@ -46,17 +49,17 @@ def reduce_to_one_answer(query: str, relevant_points: list[RelevantPointToQuery]
     non_zero_relevance_pairs = [relevant_point for relevant_point in relevant_points if relevant_point.score > 0]
     sorted_pairs = sorted(non_zero_relevance_pairs, key=lambda x: x[1], reverse=True)
     top_n_pairs = sorted_pairs[:top_n]  # TODO: make this customizable?
-    
+
     # TODO: maybe add community information as well (right now, we are only using the partial answers)
     concatenated_relevant_points = "\n".join([relevant_point.model_dump_json() for relevant_point in top_n_pairs])
-    
+
     ollama_response = generate_ollama_response(
         prompt=REDUCTION_PROMPT.format(
             query=query,
             relevant_points=concatenated_relevant_points,
         ),
     )
-    
+
     return ollama_response
 
 
@@ -64,9 +67,11 @@ def query_index(query: str, index: GraphIndex, hierachy_level: int = 1, top_n: i
     # load the index, for the particular hierachy level
     if hierachy_level not in index.hierachical_communities:
         raise ValueError(f"Invalid hierachy level: {hierachy_level}")
-    
+
     corresponding_level_communities = index.hierachical_communities[hierachy_level]
-    community_partial_answers = [map_query_to_community(query, community) for community in corresponding_level_communities]
+    community_partial_answers = [
+        map_query_to_community(query, community) for community in corresponding_level_communities
+    ]
     # just get the relevant points for now (ignore SummarizedCommunity)
     all_relevant_points = flatten([relavent_points for _, relavent_points in community_partial_answers])
     return reduce_to_one_answer(query, all_relevant_points, top_n=top_n)
