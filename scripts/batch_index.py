f157305d26a6023e43414c4428940ec9f2245985
Satoshi <6world4trigger@gmail.com>
2024-10-29 17:14:10 +0800

    support tqdm in scripts

--- scripts/batch_index.py
+++ scripts/batch_index.py
@@ -1,6 +1,7 @@
 import json
 
 import fire
+from tqdm import tqdm
 
 from trying_graph_rag.graph_rag.indexing import create_index
 
@@ -10,7 +11,7 @@ def main(test_cases_json_path: str) -> None:
     with open(test_cases_json_path, "r") as file:
         test_cases = json.load(file)
     
-    for test_case in test_cases:
+    for test_case in tqdm(test_cases, desc="Test cases"):
         document_id, documents = test_case["_id"], test_case["context"]
         create_index(documents, DEFAULT_ENTITY_TYPES, index_name=document_id)
 
--- scripts/batch_query.py
+++ scripts/batch_query.py
@@ -3,6 +3,7 @@ import pickle
 from pathlib import Path
 
 import fire
+from tqdm import tqdm
 
 from trying_graph_rag.graph_rag.querying import query_index
 
@@ -16,7 +17,7 @@ def main(test_cases_json_path: str, indexes_base_path: str, output_answers_csv_p
     with open(_ouput_answeres_csv_path, "w") as file:
         file.write("document_id,answer,explanation\n")
     
-    for test_case in test_cases:
+    for test_case in tqdm(test_cases, desc="Test cases"):
         document_id, question = test_case["_id"], test_case["question"]
         
         with open(Path(indexes_base_path) / f"{document_id}.pickle", "rb") as file:
